f2cd17a4e9d0acce2424880bdf9764fb246aee9a
Alberto <albertocruzadopleite@gmail.com>
2020-10-24 13:11:35 +0200

    The exercise was successfully completed

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
-      HOLA MUNDO
-    </div>
+    <React.Fragment>
+      <h1>Animation Input</h1>
+
+      <div className="form">
+        <input type="text" name="name" autoComplete="off" required />
+        <label for="name" className="label-name">
+          <span className="content-name">Name</span>
+        </label>
+      </div>
+    </React.Fragment>
   );
 }
 
